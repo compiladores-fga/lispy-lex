180fe655ca6a56c7b308026e25a44a2fbcbbea0e
Danillo Souza <danillosouza1704@gmail.com>
2020-10-16 10:50:12 -0300

    Lispy project done

--- lex.py
+++ lex.py
@@ -6,11 +6,30 @@ class Token(NamedTuple):
     kind: str
     value: str
 
+def createRegex():
+    token_specification = [
+        ('NUMBER', r'[-+]?\d+(\.\d*)?'),
+        ('COMMA', r','),               
+        ("STRING", r"\".*\""),
+        ("QUOTE", r"[\'\"]"),
+        ("NAME", r"[-+_\w\d\<\>\?\!\%\=\*\/\.]+"),  
+        ("CHAR", r"#\\[\w]+"),
+        ("LPAR", r"\("), 
+        ("RPAR", r"\)"), 
+        ("BOOL", r"#([fF]|[tT])"),
+        ('MISMATCH', r'.'),            
+    ]
+    regex = '|'.join('(?P<%s>%s)' % pair for pair in token_specification)
+    return regex
+        
+def lex(stringInput: str) -> Iterable[Token]:
+    
+    stringInput = stringInput.split(";;", 1)[0]
+    
+    for token in re.finditer(createRegex(), stringInput):
+        kind = token.lastgroup
+        value = token.group()
+        if kind != "MISMATCH":
+            yield Token(kind, value)
 
-def lex(code: str) -> Iterable[Token]:
-    """
-    Retorna sequência de objetos do tipo token correspondendo à análise léxica
-    da string de código fornecida.
-    """
-
-    return [Token('INVALIDA', 'valor inválido')]
+            
